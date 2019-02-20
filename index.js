a6a5b9c06dc4a036fa3570ad701c7a152488104b
ajinbramantio <ajinbramantio@gmail.com>
2019-02-20 16:15:18 +0700

    improve createn new user function

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const url = process.env.URL
 // Database Name
 const dbName = process.env.DB_NAME
 
+const createNewUser = function(db) {
+  const newUser = {
+    name: 'ajin',
+    age: 17,
+    address: 'jalan jalan'
+  }
+  db.collection('users').insert(newUser, function(err, result) {
+    console.log(result.ops)
+
+    console.log({
+      message: 'Created new user',
+      result: result
+    })
+  })
+}
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err)
@@ -15,5 +30,6 @@ MongoClient.connect(url, function(err, client) {
 
   const db = client.db(dbName)
 
+  createNewUser(db)
   client.close()
 })
